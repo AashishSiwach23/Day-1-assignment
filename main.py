9af22f3a1c7077c992259b028c29d284e6187eae
AashishSiwach23 <89123450+AashishSiwach23@users.noreply.github.com>
2021-08-18 11:50:20 +0530

    Day 1 Assignment Submitted

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hi! Sir Aashish here . It's my first day python assignment for adding two no. ")
+
+x = 25
+y = 30
+print('sum:',x+y)
+
+x = int(input('enter first no. to add :'))
+y = int(input('enter secound no. to add :'))
+print('sum:',x+y)
+
+print('thanks Sir for checking it.')
