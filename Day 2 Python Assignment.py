c12e1d540c92eb60875536b3f4327c734b10c145
AashishSiwach23 <89123450+AashishSiwach23@users.noreply.github.com>
2021-08-22 20:58:39 +0530

    Day 2 Assignment Submitted

---
+++ Day 2 Python Assignment.py
@@ -0,0 +1,22 @@
+st = 'welcome welcome come well'
+# in this assignment i am going to count the no. of repetation of every letter in the string st.
+print('w:')
+print(st.count('w'))
+
+print('e:')
+print(st.count('e'))
+
+print('l:')
+print(st.count('l'))
+
+print('c:')
+print(st.count('c'))
+
+print('o:')
+print(st.count('o'))
+
+print('m:')
+print(st.count('m'))
+
+print('e:')
+print(st.count('e'))  # Hence counted all the elements.
